e7fd00073a9d8e64ce1d4eb234d1d5fb0fede0c5
fishtankman49 <monroe_morris@Monroes-MacBook-Pro.local>
2018-11-11 13:08:51 -0900

    Timer modified servo 4

--- ServoDriver.py
+++ ServoDriver.py
@@ -11,12 +11,12 @@ print("Starting FEEDING")
 
 try:
     pwm.ChangeDutyCycle(2)
-    time.sleep(2)
+    time.sleep(.5)
     for i in range (0,95):
         pwm.ChangeDutyCycle(i)
         time.sleep(0.1)
     pwm.ChangeDutyCycle(2)
-    time.sleep(1)
+    time.sleep(.5)
     pwm.stop()
     GPIO.cleanup()
 except KeyboardInterrupt:
