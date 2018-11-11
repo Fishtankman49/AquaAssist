2ea8d07fcb71507996dce83c636f7dd28d545734
fishtankman49 <monroe_morris@Monroes-MacBook-Pro.local>
2018-11-11 11:43:29 -0900

    modified servo codeagain

--- ServoDriver.py
+++ ServoDriver.py
@@ -4,16 +4,19 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
-pwm=GPIO.PWM(7,50)
+pwm=GPIO.PWM(7,250)
 pwm.start(50)
 
 print("Starting FEEDING")
 
-pwm.ChangeDutyCycle(8)
+pwm.ChangeDutyCycle(20)
 time.sleep(1)
 
-pwm.ChangeDutyCycle(150)
-time.sleep(3)
+pwm.ChangeDutyCycle(95)
+time.sleep(1)
+
+pwm.ChangeDutyCycle(20)
+time.sleep(1)
 
 pwm.stop()
 GPIO.cleanup()
