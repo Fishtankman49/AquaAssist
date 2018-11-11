17ed5f0bc0f7e10851c0c5b8758c13938b761d4c
fishtankman49 <monroe_morris@Monroes-MacBook-Pro.local>
2018-11-11 11:31:46 -0900

    added servo code

---
+++ ServoDriver.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+
+pwm=GPIO.PWM(7,50)
+pwm.start(50)
+
+print("Starting FEEDING")
+
+pwm.ChangeDutyCycle(8)
+time.sleep(1)
+
+pwm.ChangeDutyCycle(95)
+time.sleep(3)
